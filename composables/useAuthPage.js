2000858c523f6385821e5543da8ff0617d559f10
AMRWERY <amrmounir2@gmail.com>
2025-01-23 22:52:23 +0200

    .

--- composables/useAuthPage.js
+++ composables/useAuthPage.js
@@ -2,7 +2,7 @@ export const useAuthPage = () => {
   const route = useRoute();
 
   const isAuthPage = computed(() =>
-    ["/register", "/forget-password", "/admin-login"].includes(route.path)
+    ["/sign-up", "/dashboard/login"].includes(route.path)
   );
 
   return { isAuthPage };
