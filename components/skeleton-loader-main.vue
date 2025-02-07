1a216f8d70f64db2158b0a4e25cbd490c214e1f5
AMRWERY <amrmounir2@gmail.com>
2025-02-07 22:38:51 +0200

    .

--- components/skeleton-loader-main.vue
+++ components/skeleton-loader-main.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-200 animate-pulse">
-      <!-- <p class="text-5xl font-bold text-gray-500">Ship-IT...</p> -->
       <img src="@/public/shopping-bags.svg" alt="shopping-bags" class="w-64 h-64 mt-3 text-gray-500 ms-2">
     </div>
   </div>
