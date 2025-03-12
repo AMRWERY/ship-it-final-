445c5959e90cfcfad0ed48cfb3a6d6a92d5d9164
AMRWERY <amrmounir2@gmail.com>
2025-03-12 14:09:54 +0200

    .

--- components/reusable/product-cards.vue
+++ components/reusable/product-cards.vue
@@ -82,8 +82,8 @@
     <!-- dynamic-toast component -->
     <div class="fixed z-50 pointer-events-none bottom-5 start-5 w-96">
       <div class="pointer-events-auto">
-        <dynamic-toast v-if="showToast" :message="toastMessage" :toastType="toastType"
-          :duration="5000" :toastIcon="toastIcon" @toastClosed="showToast = false" />
+        <dynamic-toast v-if="showToast" :message="toastMessage" :toastType="toastType" :duration="5000"
+          :toastIcon="toastIcon" @toastClosed="showToast = false" />
       </div>
     </div>
   </div>
