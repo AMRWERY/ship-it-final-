74cefd6a91374aa8ecb77d4675fcc48e9ec7b2b8
AMRWERY <amrmounir2@gmail.com>
2025-03-12 16:48:40 +0200

    .

--- stores/themeStore.js
+++ stores/themeStore.js
@@ -1,34 +1,31 @@
-export const useThemeStore = defineStore("theme", () => {
-  const isDark = ref(false);
-  const showOverlay = ref(false);
+export const useThemeStore = defineStore("theme", {
+  state: () => ({
+    isDark: false,
+    showOverlay: false,
+  }),
 
-  const toggleTheme = () => {
-    showOverlay.value = true;
-    setTimeout(() => {
-      isDark.value = !isDark.value;
-      if (isDark.value) {
+  actions: {
+    toggleTheme() {
+      this.showOverlay = true;
+      setTimeout(() => {
+        this.isDark = !this.isDark;
+        if (this.isDark) {
+          document.body.classList.add("dark");
+          localStorage.setItem("theme", "dark");
+        } else {
+          document.body.classList.remove("dark");
+          localStorage.setItem("theme", "light");
+        }
+        this.showOverlay = false;
+      }, 3000);
+    },
+
+    loadTheme() {
+      const savedTheme = localStorage.getItem("theme");
+      this.isDark = savedTheme === "dark";
+      if (this.isDark) {
         document.body.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-      } else {
-        document.body.classList.remove("dark");
-        localStorage.setItem("theme", "light");
       }
-      showOverlay.value = false;
-    }, 3000);
-  };
-
-  const loadTheme = () => {
-    const savedTheme = localStorage.getItem("theme");
-    isDark.value = savedTheme === "dark";
-    if (isDark.value) {
-      document.body.classList.add("dark");
-    }
-  };
-
-  return {
-    isDark,
-    showOverlay,
-    toggleTheme,
-    loadTheme,
-  };
+    },
+  },
 });
