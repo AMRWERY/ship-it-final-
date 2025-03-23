7c3bbebe7c3769e1f92b539c25ca5f2c7f8de920
AMRWERY <amrmounir2@gmail.com>
2025-03-23 21:29:51 +0200

    .

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -349,12 +349,9 @@ const currentBrand = computed(() => route.query.brand)
 
 const brandName = computed(() => {
   if (route.query.brand) {
-    // return route.query.brand.replace(/_/g, ' ');
-    return locale.value === 'ar'
-    ? route.query.brandAr
-    : route.query.brand;;
+    return route.query.brand.replace(/_/g, ' ');
   }
-  
+
   if (route.query.categoryId) return locale.value === 'ar'
     ? route.query.titleAr
     : route.query.title;;
