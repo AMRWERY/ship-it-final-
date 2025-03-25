b9a81b58e1d8960627a8dc4edf76aa2f2ab30c0d
AMRWERY <amrmounir2@gmail.com>
2025-03-26 01:15:28 +0200

    handle add spinner icon when open dialog & translate blogs component

--- components/menus-and-dialogs/blogs-dialog.vue
+++ components/menus-and-dialogs/blogs-dialog.vue
@@ -1,37 +1,31 @@
-<!-- BlogsDialog.vue -->
 <template>
   <div>
     <div v-if="blog" class="fixed inset-0 z-[1000] flex items-center justify-center p-4 bg-black bg-opacity-50">
       <div class="relative w-full max-w-3xl max-h-[90vh] overflow-auto bg-white dark:bg-[#181a1b] rounded-lg shadow-lg">
         <div class="p-6">
           <div class="relative group">
-            <button 
-              @click="$emit('close')" 
-              class="absolute top-2 end-2 z-10 p-1.5 transition-colors rounded-full bg-white/80 hover:bg-white/90 text-gray-700 hover:text-red-500 dark:bg-gray-700/80 dark:hover:bg-gray-600/90 dark:text-gray-200"
-            >
+            <button @click="$emit('close')"
+              class="absolute top-2 end-2 z-10 p-1.5 transition-colors rounded-full bg-white/80 hover:bg-white/90 text-gray-700 hover:text-red-500 dark:bg-gray-700/80 dark:hover:bg-gray-600/90 dark:text-gray-200">
               <icon name="material-symbols:close-small-rounded" class="flex items-center w-7 h-7" />
             </button>
-            
-            <img 
-              :src="blog.img" 
-              :alt="blog.title" 
-              class="object-cover w-full h-64 mb-4 rounded-lg shadow-md dark:shadow-gray-900"
-            >
+
+            <img :src="blog.img" :alt="blog.title"
+              class="object-cover w-full h-64 mb-4 rounded-lg shadow-md dark:shadow-gray-900">
           </div>
 
           <div class="flex items-center mb-4 text-sm text-gray-500 dark:text-gray-300">
             <span class="me-4">{{ blog.date }}</span>
           </div>
-          
+
           <h2 class="mb-4 text-2xl font-bold text-gray-800 dark:text-gray-200">{{ blog.title }}</h2>
-          
+
           <p class="mb-3 text-lg text-gray-600 dark:text-gray-300">{{ blog.desc }}</p>
 
-          <!-- <div class="space-y-4 text-gray-700 dark:text-gray-300">
+          <div class="space-y-4 text-gray-700 dark:text-gray-300">
             <p v-for="(paragraph, index) in blog.content" :key="index" class="leading-relaxed">
               {{ paragraph }}
             </p>
-          </div> -->
+          </div>
         </div>
       </div>
     </div>
--- pages/blogs.vue
+++ pages/blogs.vue
@@ -3,32 +3,36 @@
     <div class="py-6 mt-4 rounded-lg shadow-lg sm:py-8 lg:py-12 dark:border dark:border-gray-100">
       <div class="max-w-screen-xl px-4 mx-auto md:px-8">
         <div class="mb-10 md:mb-16">
-          <h2 class="mb-4 text-2xl font-bold text-center text-gray-800 md:mb-6 lg:text-3xl dark:text-gray-200">Shipping
-            Insights & Updates
+          <h2 class="mb-4 text-2xl font-bold text-center text-gray-800 md:mb-6 lg:text-3xl dark:text-gray-200">{{
+            $t('blogs.shipping_insights_updates') }}
           </h2>
-          <p class="max-w-screen-md mx-auto text-center text-gray-500 md:text-lg dark:text-gray-100">Stay informed with
-            the latest trends,
-            tips, and innovations in global logistics and e-commerce shipping.</p>
+          <p class="max-w-screen-sm mx-auto text-center text-gray-500 md:text-lg dark:text-gray-100">{{
+            $t('blogs.stay_informed_with_the_latest_trends_tips_and_innovations_in_global_logistics_and_ecommerce_shipping')
+            }}</p>
         </div>
 
         <div class="grid gap-8 sm:grid-cols-2 sm:gap-12 lg:grid-cols-2 xl:grid-cols-2 xl:gap-16">
           <div class="flex flex-col items-center gap-4 md:flex-row lg:gap-6" v-for="blog in blogs" :key="blog">
-            <router-link to=""
+            <nuxt-link to=""
               class="relative self-start block w-full h-56 overflow-hidden bg-gray-100 rounded-lg shadow-lg group shrink-0 md:h-24 md:w-24 lg:h-40 lg:w-40">
               <img :src="blog.img" loading="lazy" alt="Shipping technology"
                 class="absolute inset-0 object-cover object-center w-full h-full transition duration-200 group-hover:scale-110" />
-            </router-link>
+            </nuxt-link>
             <div class="flex flex-col gap-2">
               <span class="text-sm text-gray-400 dark:text-gray-100">{{ blog.date }}</span>
               <h2 class="text-xl font-bold text-gray-800 dark:text-gray-200">
-                <router-link to="" class="transition duration-100 hover:text-blue-600 active:text-blue-700">{{
-                  blog.title }}</router-link>
+                <nuxt-link to="" class="transition duration-100 hover:text-blue-600 active:text-blue-700">{{
+                  blog.title }}</nuxt-link>
               </h2>
               <p class="text-gray-500 dark:text-gray-100">{{ blog.desc }}</p>
               <div class="mt-2">
                 <button role="button" @click="openBlog(blog)"
-                  class="text-sm text-blue-700 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-500">{{
-                    $t('btn.open_blog') }}</button>
+                  class="text-sm text-blue-700 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-500">
+                  <icon name="svg-spinners:270-ring-with-bg" v-if="isLoading === blog.id" />
+                  <span v-else>
+                    {{ $t('btn.open_blog') }}
+                  </span>
+                </button>
               </div>
               <div>
               </div>
@@ -36,9 +40,9 @@
           </div>
 
           <!-- blogs-dialog component -->
-          <ClientOnly>
+          <transition name="fade" appear>
             <blogs-dialog v-if="selectedBlog" :blog="selectedBlog" @close="closeBlog" />
-          </ClientOnly>
+          </transition>
         </div>
       </div>
     </div>
@@ -46,10 +50,16 @@
 </template>
 
 <script setup>
+const { t } = useI18n()
 const selectedBlog = ref(null)
+const isLoading = ref(null)
 
 const openBlog = (blog) => {
-  selectedBlog.value = blog
+  isLoading.value = blog.id
+  setTimeout(() => {
+    selectedBlog.value = blog
+    isLoading.value = false
+  }, 2000)
 }
 
 const closeBlog = () => {
@@ -58,28 +68,64 @@ const closeBlog = () => {
 
 const blogs = computed(() => [
   {
-    date: 'March 15, 2024',
-    title: '5 Ways to Optimize Your Shipping Workflow',
-    desc: 'Discover how smart packaging choices and route optimization can reduce costs by up to 30%.',
-    img: 'https://justfields.com/storage/projects/7M5rV059/blog-photo.jpg'
+    id: 1,
+    date: t('blogs.date_1'),
+    title: t('blogs.title_1'),
+    desc: t('blogs.desc_1'),
+    img: 'https://justfields.com/storage/projects/7M5rV059/blog-photo.jpg',
+    content: [
+      t('blogs.paragraph_1'),
+      t('blogs.paragraph_2'),
+      t('blogs.paragraph_3')
+    ]
   },
   {
-    date: 'March 12, 2024',
-    title: 'Mastering International Shipping Regulations',
-    desc: 'Essential guide to navigating customs, duties, and cross-border logistics.',
-    img: 'https://justfields.com/storage/projects/7M5rV059/post-2.jpg'
+    id: 2,
+    date: t('blogs.date_2'),
+    title: t('blogs.title_2'),
+    desc: t('blogs.desc_2'),
+    img: 'https://justfields.com/storage/projects/7M5rV059/post-1.jpg',
+    content: [
+      t('blogs.paragraph_4'),
+      t('blogs.paragraph_5'),
+      t('blogs.paragraph_6')
+    ]
   },
   {
-    date: 'March 8, 2024',
-    title: 'Sustainable Shipping: Our Green Initiative',
-    desc: "How we're reducing carbon footprint with biodegradable materials and electric fleets.",
-    img: 'https://justfields.com/storage/projects/7M5rV059/post-1.jpg'
+    id: 3,
+    date: t('blogs.date_3'),
+    title: t('blogs.title_3'),
+    desc: t('blogs.desc_3'),
+    img: 'https://justfields.com/storage/projects/7M5rV059/post-1.jpg',
+    content: [
+      t('blogs.paragraph_7'),
+      t('blogs.paragraph_8'),
+      t('blogs.paragraph_9')
+    ]
   },
   {
-    date: 'March 1, 2024',
-    title: 'Real-Time Tracking: Enhancing Customer Experience',
-    desc: "Learn how our new tracking system improves delivery transparency and satisfaction.",
-    img: 'https://justfields.com/storage/projects/7M5rV059/blog-img.jpg'
+    id: 4,
+    date: t('blogs.date_4'),
+    title: t('blogs.title_4'),
+    desc: t('blogs.desc_4'),
+    img: 'https://justfields.com/storage/projects/7M5rV059/blog-img.jpg',
+    content: [
+      t('blogs.paragraph_10'),
+      t('blogs.paragraph_11'),
+      t('blogs.paragraph_12')
+    ]
   }
 ]);
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
