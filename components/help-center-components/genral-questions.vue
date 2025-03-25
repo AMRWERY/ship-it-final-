3fd5ec648833570de9ffa76504330cb19459bbc6
AMRWERY <amrmounir2@gmail.com>
2025-03-25 22:44:40 +0200

    finish translation help-center page

--- components/help-center-components/genral-questions.vue
+++ components/help-center-components/genral-questions.vue
@@ -1,72 +1,61 @@
 <template>
   <div>
     <div class="p-5 mx-auto border border-gray-200 rounded-lg shadow-lg max-w-7xl">
-      <h2 class="text-2xl font-bold text-center mb-7">General Questions</h2>
+      <h2 class="text-2xl font-bold text-center mb-7">{{ $t('help_center.general_questions') }}</h2>
       <div class="space-y-6">
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">1-</span>What is Ship‑IT?
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">1-</span>{{
+            $t('help_center.what_is_ship_it') }}
           </p>
-          <span class="text-base text-gray-600 dark:text-gray-100">Ship‑IT is your one‑stop online destination for
-            high‑quality products at competitive prices. Our mission is to make shopping simple, convenient, and secure.
-            Whether you’re looking for everyday essentials or special items, we’re here to help you find what you
-            need.</span>
+          <span class="text-base text-gray-600 dark:text-gray-100">{{ $t('help_center.what_is_ship_it_desc') }}</span>
         </div>
 
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">2-</span>How do I navigate
-            the website?
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">2-</span>{{
+            $t('help_center.how_do_i_navigate_the_website') }}
           </p>
-          <span class="text-base text-gray-600 dark:text-gray-100">Our website is designed with user‑friendliness in
-            mind. Use the search bar to quickly find products, browse categories via the sidebar, and take advantage of
-            our detailed product pages. If you have any trouble navigating the site, check out our FAQ sections or
-            contact customer support for guidance.</span>
+          <span class="text-base text-gray-600 dark:text-gray-100">{{
+            $t('help_center.how_do_i_navigate_the_website_desc') }}</span>
         </div>
 
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">3-</span>How can I get help
-            if I have a question or issue?
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">3-</span>{{
+            $t('help_center.how_can_i_get_help_if_i_have_a_question_or_issue') }}
           </p>
-          <span class="text-base text-gray-600 dark:text-gray-100">If you have questions that are not answered in our
-            help center, please visit the Contact Support page. You can also reach out via our live chat or email our
-            customer service team. We’re committed to ensuring that your experience is smooth and enjoyable.</span>
+          <span class="text-base text-gray-600 dark:text-gray-100">{{
+            $t('help_center.how_can_i_get_help_if_i_have_a_question_or_issue_desc') }}</span>
         </div>
 
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">4-</span>What should I do if
-            I encounter technical issues?
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">4-</span>{{
+            $t('help_center.what_should_i_do_if_i_encounter_technical_issues') }}
           </p>
-          <span class="text-base text-gray-600 dark:text-gray-100">If you experience any technical issues while browsing
-            or shopping (such as slow page loads or broken links), try refreshing the page or clearing your browser’s
-            cache. If problems persist, please contact our technical support team through our Contact Support
-            page.</span>
+          <span class="text-base text-gray-600 dark:text-gray-100">{{
+            $t('help_center.what_should_i_do_if_i_encounter_technical_issues_desc') }}</span>
         </div>
 
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">5-</span>Is my personal
-            information secure?
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">5-</span>{{
+            $t('help_center.is_my_personal_information_secure') }}
           </p>
-          <span class="text-base text-gray-600 dark:text-gray-100">Yes. Ship‑IT uses industry‑standard security measures
-            to protect your personal and payment information. We continuously update our security protocols to ensure
-            that your data is safe, and our privacy policy provides more details about how we manage and protect your
-            information.</span>
+          <span class="text-base text-gray-600 dark:text-gray-100">{{
+            $t('help_center.is_my_personal_information_secure_desc') }}</span>
         </div>
 
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">6-</span>How often do you
-            update your product selection?
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">6-</span>{{
+            $t('help_center.how_often_do_you_update_your_product_selection') }}
           </p>
-          <span class="text-base text-gray-600 dark:text-gray-100">Our inventory is updated frequently to ensure we
-            always offer the latest trends and best deals. Check back regularly for new arrivals and seasonal
-            promotions.</span>
+          <span class="text-base text-gray-600 dark:text-gray-100">{{
+            $t('help_center.how_often_do_you_update_your_product_selection_desc') }}</span>
         </div>
 
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">7-</span>Can I change my
-            language preference?
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">7-</span>{{
+            $t('help_center.can_i_change_my_language_preference') }}
           </p>
-          <span class="text-base text-gray-600 dark:text-gray-100">Yes. Our website supports multiple languages. You can
-            change your language preference using the language switcher available in the header of the site. This helps
-            you navigate and understand our content in the language you’re most comfortable with.</span>
+          <span class="text-base text-gray-600 dark:text-gray-100">{{
+            $t('help_center.can_i_change_my_language_preference_desc') }}</span>
         </div>
       </div>
     </div>
--- components/help-center-components/payment-issues.vue
+++ components/help-center-components/payment-issues.vue
@@ -1,48 +1,44 @@
 <template>
   <div>
     <div class="p-5 mx-auto border border-gray-200 rounded-lg shadow-lg max-w-7xl">
-      <h2 class="text-2xl font-bold text-center mb-7">Payment Issues</h2>
+      <h2 class="text-2xl font-bold text-center mb-7">{{ $t('help_center.Payment_issues') }}</h2>
       <div class="space-y-6">
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">1-</span>Common Payment
-            Problems
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">1-</span>{{
+            $t('help_center.common_payment_problems') }}
           </p>
           <span class="text-base text-gray-600 dark:text-gray-100">
             <ul class="space-y-2 list-none">
               <li
                 class="flex items-start text-gray-700 dark:text-gray-300 before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-gray-800 dark:before:bg-gray-200 before:rounded-full before:me-2 before:mt-2">
                 <span class="flex-1">
-                  <span class="font-semibold text-blue-600 dark:text-blue-400">Payment Declined:</span>
-                  <p>Sometimes, transactions may be declined by your bank or card issuer due to insufficient funds,
-                    incorrect card details, or fraud protection measures. Verify that your card information is correct
-                    and that there are sufficient funds available.</p>
+                  <span class="font-semibold text-blue-600 dark:text-blue-400">{{ $t('help_center.payment_declined')
+                    }}</span>
+                  <p>{{ $t('help_center.payment_declined_desc') }}</p>
                 </span>
               </li>
               <li
                 class="flex items-start text-gray-700 dark:text-gray-300 before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-gray-800 dark:before:bg-gray-200 before:rounded-full before:me-2 before:mt-2">
                 <span class="flex-1">
-                  <span class="font-semibold text-blue-600 dark:text-blue-400">Failed Transaction:</span>
-                  <p>In some cases, your transaction may fail due to network issues or temporary problems with your
-                    payment provider. If you experience this, try the payment again after a few minutes.</p>
+                  <span class="font-semibold text-blue-600 dark:text-blue-400">{{ $t('help_center.failed_transaction')
+                  }}</span>
+                  <p>{{ $t('help_center.failed_transaction_desc') }}</p>
                 </span>
               </li>
               <li
                 class="flex items-start text-gray-700 dark:text-gray-300 before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-gray-800 dark:before:bg-gray-200 before:rounded-full before:me-2 before:mt-2">
                 <span class="flex-1">
-                  <span class="font-semibold text-blue-600 dark:text-blue-400">Double Charges or Incorrect
-                    Amounts:</span>
-                  <p>If you notice that you’ve been charged twice or that the amount billed doesn’t match your order,
-                    please contact your bank immediately. We recommend keeping a copy of your order confirmation and
-                    transaction details, and then reach out to our customer support for assistance.</p>
+                  <span class="font-semibold text-blue-600 dark:text-blue-400">{{
+                    $t('help_center.double_charges_or_incorrect_amounts') }}</span>
+                  <p>{{ $t('help_center.double_charges_or_incorrect_amounts_desc') }}</p>
                 </span>
               </li>
               <li
                 class="flex items-start text-gray-700 dark:text-gray-300 before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-gray-800 dark:before:bg-gray-200 before:rounded-full before:me-2 before:mt-2">
                 <span class="flex-1">
-                  <span class="font-semibold text-blue-600 dark:text-blue-400">Currency Conversion Issues:</span>
-                  <p>If you’re making an international purchase, your card issuer might convert the charge to your local
-                    currency. This could result in an unexpected amount due to exchange rates or fees. Please check with
-                    your bank for details.</p>
+                  <span class="font-semibold text-blue-600 dark:text-blue-400">{{
+                    $t('help_center.currency_conversion_issues') }}</span>
+                  <p>{{ $t('help_center.currency_conversion_issues_desc') }}</p>
                 </span>
               </li>
             </ul>
@@ -50,41 +46,41 @@
         </div>
 
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">2-</span>What to Do If You
-            Encounter Payment Issues
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">2-</span>{{
+            $t('help_center.what_to_do_if_you_encounter_payment_issues') }}
           </p>
           <span class="text-base text-gray-600 dark:text-gray-100">
             <ul class="space-y-2 list-none">
               <li
                 class="flex items-start text-gray-700 dark:text-gray-300 before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-gray-800 dark:before:bg-gray-200 before:rounded-full before:me-2 before:mt-2">
                 <span class="flex-1">
-                  <span class="font-semibold text-blue-600 dark:text-blue-400">Review Your Payment Details:</span>
-                  <p>Ensure that your card number, expiration date, CVV, and billing address are entered correctly.</p>
+                  <span class="font-semibold text-blue-600 dark:text-blue-400">{{
+                    $t('help_center.what_to_do_if_you_encounter_payment_issues_desc') }}</span>
+                  <p>{{ $t('help_center.what_to_do_if_you_encounter_payment_issues_desc_1') }}</p>
                 </span>
               </li>
               <li
                 class="flex items-start text-gray-700 dark:text-gray-300 before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-gray-800 dark:before:bg-gray-200 before:rounded-full before:me-2 before:mt-2">
                 <span class="flex-1">
-                  <span class="font-semibold text-blue-600 dark:text-blue-400">Try an Alternative Payment Method:</span>
-                  <p>If your preferred payment method fails, consider using an alternative payment option if available,
-                    such as another card or an online payment service.</p>
+                  <span class="font-semibold text-blue-600 dark:text-blue-400">{{
+                    $t('help_center.what_to_do_if_you_encounter_payment_issues_desc_2') }}</span>
+                  <p>{{ $t('help_center.what_to_do_if_you_encounter_payment_issues_desc_3') }}</p>
                 </span>
               </li>
               <li
                 class="flex items-start text-gray-700 dark:text-gray-300 before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-gray-800 dark:before:bg-gray-200 before:rounded-full before:me-2 before:mt-2">
                 <span class="flex-1">
-                  <span class="font-semibold text-blue-600 dark:text-blue-400">Contact Your Bank:</span>
-                  <p>Sometimes, banks may block transactions as a precaution. Contact your bank to confirm that your
-                    card is active and that there are no holds on your account.</p>
+                  <span class="font-semibold text-blue-600 dark:text-blue-400">{{
+                    $t('help_center.what_to_do_if_you_encounter_payment_issues_desc_4') }}</span>
+                  <p>{{ $t('help_center.what_to_do_if_you_encounter_payment_issues_desc_5') }}</p>
                 </span>
               </li>
               <li
                 class="flex items-start text-gray-700 dark:text-gray-300 before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-gray-800 dark:before:bg-gray-200 before:rounded-full before:me-2 before:mt-2">
                 <span class="flex-1">
-                  <span class="font-semibold text-blue-600 dark:text-blue-400">Reach Out to Customer Support:</span>
-                  <p>If you’ve verified your payment details and contacted your bank but the issue persists, please
-                    contact our support team. Provide your order number, the payment method used, and a description of
-                    the issue. Our support team is here to help resolve any payment problems promptly.</p>
+                  <span class="font-semibold text-blue-600 dark:text-blue-400">{{
+                    $t('help_center.what_to_do_if_you_encounter_payment_issues_desc_6') }}</span>
+                  <p>{{ $t('help_center.what_to_do_if_you_encounter_payment_issues_desc_7') }}</p>
                 </span>
               </li>
             </ul>
@@ -92,11 +88,10 @@
         </div>
 
         <div>
-          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">3-</span>Need Further
-            Assistance?</p>
-          <span class="text-base text-gray-600 dark:text-gray-100">Visit our Contact Support page or call our customer
-            service hotline. We’re committed to resolving payment issues quickly so that you can enjoy a seamless
-            shopping experience.</span>
+          <p class="mb-2 font-semibold text-gray-800 dark:text-gray-200"><span class="me-1">3-</span>{{
+            $t('help_center.need_further_assistance') }}</p>
+          <span class="text-base text-gray-600 dark:text-gray-100">{{
+            $t('help_center.what_to_do_if_you_encounter_payment_issues_desc_8') }}</span>
         </div>
       </div>
     </div>
